aa92f44c66b9c06f625c760748b061fcaae316c3
Emmanuel Pallares <emmanuelpallares50@gmail.com>
2022-02-27 17:39:28 -0600

    add a endpoint public for vercel

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,34 +1,35 @@
-require('dotenv').config()
+require("dotenv").config();
 
 module.exports = {
   siteMetadata: {
-    title: 'GraphCMS Blog',
+    title: "GraphCMS Blog",
     description:
-      'Gatsby blog starter for GraphCMS! Powered by `gatsby-source-graphcms`, featuring `gatsby-image` and MDX!',
-    keywords: 'Headless CMS, GraphCMS, GraphQL CMS, Gatsby',
+      "Gatsby blog starter for GraphCMS! Powered by `gatsby-source-graphcms`, featuring `gatsby-image` and MDX!",
+    keywords: "Headless CMS, GraphCMS, GraphQL CMS, Gatsby",
   },
   plugins: [
-    'gatsby-plugin-mdx',
+    "gatsby-plugin-mdx",
     {
-      resolve: 'gatsby-plugin-react-svg',
+      resolve: "gatsby-plugin-react-svg",
       ptions: {
         rule: {
           include: /svg/,
         },
       },
     },
-    'gatsby-plugin-react-helmet',
-    'gatsby-plugin-sharp',
-    'gatsby-plugin-postcss',
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sharp",
+    "gatsby-plugin-postcss",
     {
-      resolve: 'gatsby-source-graphcms',
+      resolve: "gatsby-source-graphcms",
       options: {
-        endpoint: process.env.GRAPHCMS_ENDPOINT,
+        endpoint:
+          "https://api-us-west-2.graphcms.com/v2/cl05und8k1q1e01yzaogr7bli/master",
         token: process.env.GRAPHCMS_TOKEN,
         buildMarkdownNodes: true,
         downloadLocalImages: true,
       },
     },
-    'gatsby-transformer-sharp',
+    "gatsby-transformer-sharp",
   ],
-}
+};
